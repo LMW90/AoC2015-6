34f88cc33ff5b5119c0ea1ce2e0da33d84727399
Łukasz Wargocki <lmwargocki@gmail.com>
2022-05-09 12:30:05 +0200

    first draft

--- AoC2015-6.py
+++ AoC2015-6.py
@@ -1,3 +1,22 @@
-tut = (8, 2, 1)
 
-print(tut[0])
+# class light():
+#     # TODO: object holding it's coordinates and "on" True/False status
+    
+# def toggle ():
+#     # TODO: function running bitwise NOT on lights status
+    
+# def turn_off ():
+#     # function setting light status to false
+    
+
+# def turn_on ():
+#     # function setting light status to true
+    
+"""
+TODO: read input for each line run startswith() to determine which function call
+and for which lights
+data structure could just be an array of dictionaries with "x", "y", "status" properties
+"""
+filename = "inputDay-6.txt"
+fhand = open(filename)
+print(fhand)
