393e0a6823a999b8059a172abd65db0cf0c8b39b
Lukasz Wargocki <lmwargocki@gmail.com>
2022-05-09 15:08:31 +0200

    added collecting input plus draft of function selection logic

--- AoC2015-6.py
+++ AoC2015-6.py
@@ -22,4 +22,19 @@ of dictionaries with "x", "y", "status" properties
 
 filename = "inputDay-6.txt"
 fhand = open(filename)
-print(fhand)
+# print(fhand)
+textInput = ''
+for line in fhand:
+    textInput += line
+fhand.close()
+# print(textInput)
+# make it so first comma-separated pair of ints sets starting range for loops
+# (^)(\d+,\d+)*(\d+,+d+)
+# for line in textInput:
+    # regex.findall(line)
+    # if line.startswith('turn on'):
+        #  turn_on(groups[1], groups[2])
+    #  elif line.startswith('turn off'):
+    #     turn_off(groups[1], groups[2])
+    # else:
+    #     toggle(groups[1], groups[2])
