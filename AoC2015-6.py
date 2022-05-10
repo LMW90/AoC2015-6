0d2916e2594cfd278f70cd3beb099ae313e38b3f
Łukasz Wargocki <lmwargocki@gmail.com>
2022-05-10 16:51:59 +0200

    defined list for lights and light object

--- AoC2015-6.py
+++ AoC2015-6.py
@@ -1,25 +1,34 @@
 
-# class light():
-#     # TODO: object holding it's coordinates and "on" True/False status
-    
-# def toggle ():
-#     # TODO: function running bitwise NOT on lights status
-    
-# def turn_off ():
-#     # function setting light status to false
+class Light: 
+    def __init__(self, x, y, val=False):
+        self.x, self.y, self.status = x, y, val
+    def turn_on(self):
+        self.status = True
+    def turn_off(self):
+        self.status = False
+    def toggle(self):
+        if self.status == True:
+            self.status = False
+        else:
+            self.status = True
+            
     
 
-# def turn_on ():
-#     # function setting light status to true
-    
 """
-TODO: read input for each line run startswith() to determine which function call
+TODO: read input for each line run startswith() 
+to determine which function call
 and for which lights
 data structure could just be an array 
 of dictionaries with "x", "y", "status" properties
 """
-
-
+# load the input
 filename = "inputDay-6.txt"
 fhand = open(filename)
 print(fhand)
+# create and populate a list
+lights = []
+for i in range(0, 1000):
+    for j in range(0, 1000):
+        lights.append([i, j , False])
+print(lights[999999])
+print(len(lights))
