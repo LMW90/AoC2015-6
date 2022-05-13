7cce87f27510f59b00f04472cf0a70d977344102
Lukasz Wargocki <lmwargocki@gmail.com>
2022-05-13 20:27:00 +0200

    part one done

--- AoC2015-6.py
+++ AoC2015-6.py
@@ -1,35 +1,13 @@
 import re
-class Light: 
-    def __init__(self, x, y, val=False):
-        self.x, self.y, self.status = x, y, val
-    def turn_on(self):
-        self.status = True
-    def turn_off(self):
-        self.status = False
-    def toggle(self):
-        if self.status == True:
-            self.status = False
-        else:
-            self.status = True
-            
-"""
-TODO: read input for each line run startswith() 
-to determine which function call
-and for which lights
-data structure could just be an array 
-of dictionaries with "x", "y", "status" properties
-"""
-# load the input
-filename = "inputDay-6.txt"
-fhand = open(filename)
-# print(fhand)
+
+
 # create and populate a list
-tmp = [False] * 1000
-lights = [tmp] * 1000
+lights = [[False for i in range(1000)] for j in range(1000)]
 # print(lights)
 # print(len(lights))
 # print(len(lights[0]))
 # print(lights[5][300])
+# load the input
 filename = 'inputDay-6.txt'
 fhand = open(filename, 'r')
 coord = re.compile(r'\d{1,3},\d{1,3}')
@@ -49,25 +27,25 @@ for line in fhand:
     # print(endX)
     # print(endY)
     if line.startswith("turn on"):
-        for i in range(startX, endX):
-            for j in range(startY, endY):
-                lights[i][j] = True
+        for y in range(startX, endX):
+            for x in range(startY, endY):
+                lights[y][x] = True
     elif line.startswith("turn off"):
-         for i in range(startX, endX):
-            for j in range(startY, endY):       
-                lights[i][j] = False
+         for y in range(startX, endX):
+            for x in range(startY, endY):       
+                lights[y][x] = False
     elif line.startswith("toggle"):
-           for i in range(startX, endX):
-            for j in range(startY, endY):    
-                if lights[i][j] == False:
-                    lights[i][j] = True
+           for y in range(startX, endX):
+            for x in range(startY, endY):    
+                if lights[y][x] == False:
+                    lights[y][x] = True
                 else:
-                    lights[i][j] = False
+                    lights[y][x] = False
 fhand.close()
 counter = 0
-for i in range(0, 1000):
-    for j in range(0, 1000):
-        if lights[i][j] == True:
+for y in range(0, 1000):
+    for x in range(0, 1000):
+        if lights[y][x] == True:
             counter += 1
 print(counter)
 
